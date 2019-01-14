530c5fe356a7cda76dd8dcb8e8aa5b92ebcc3b2b
pangprp <46663581+pangprp@users.noreply.github.com>
2019-01-14 11:59:10 +0700

    Update botpush.php

--- Desktop/line-bot-unlimit_friend-master/botpush.php
+++ Desktop/line-bot-unlimit_friend-master/botpush.php
@@ -4,7 +4,7 @@
 
 require "vendor/autoload.php";
 
-$access_token = '*Channel_access_token*';
+$access_token = 'L7kLmbLtQ8tNg8+Z/+DTa1XptJUt9H6sqE6ILisu+HftoEoL+LQhdR6rIpLppGgy+8s+0TrCkV+tsmkViIZeSVEMewE8GYR0x9QohtDnSaMFhHeknWc31yGtBIy4HoKbZrxg0XP0/dteZmwc1ACfagdB04t89/1O/w1cDnyilFU=';
 
 $channelSecret = '*Channel_Secret*';
 // ID ที่ต้องการส่งไปหา
