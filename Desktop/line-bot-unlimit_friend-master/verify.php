745f9a3517128592978b7d82f0146fc269a0fa32
pangprp <46663581+pangprp@users.noreply.github.com>
2019-01-14 11:31:29 +0700

    Update verify.php

--- Desktop/line-bot-unlimit_friend-master/verify.php
+++ Desktop/line-bot-unlimit_friend-master/verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '*Channel_access_token*';
+$access_token = 'L7kLmbLtQ8tNg8+Z/+DTa1XptJUt9H6sqE6ILisu+HftoEoL+LQhdR6rIpLppGgy+8s+0TrCkV+tsmkViIZeSVEMewE8GYR0x9QohtDnSaMFhHeknWc31yGtBIy4HoKbZrxg0XP0/dteZmwc1ACfagdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
